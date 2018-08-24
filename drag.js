b825baa7c1558b6a1834e52e1f259afe0b2e09b5
jj1992 <jjj_1992@163.com>
2018-08-24 11:26:42 +0800

    opacity

--- drag.js
+++ drag.js
@@ -1,6 +1,7 @@
 function drag(id){
 	var ele = document.getElementById(id);
-	ele.onmousedown = function(){
-		
+	ele.onmousedown = function(evt){
+		var e = evt || window.event;
+		var disX = e.offsetX;
 	}
 }
